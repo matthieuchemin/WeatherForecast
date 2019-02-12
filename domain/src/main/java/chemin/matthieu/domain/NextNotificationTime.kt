bc333b3604c0b95c79a95881208a7cdb29fc84f6
Matthieu Chemin <matthieu.chemin@adeline.mobi>
2019-02-12 07:28:42 +0100

    fix next notification time use case

--- domain/src/main/java/chemin/matthieu/domain/NextNotificationTime.kt
+++ domain/src/main/java/chemin/matthieu/domain/NextNotificationTime.kt
@@ -2,12 +2,19 @@ package chemin.matthieu.domain
 
 import org.apache.commons.lang3.time.DateUtils
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class NextNotificationTime : UseCase<Long, Long>() {
 
     override fun perform(input: Long): Long {
         var date = Date(input)
         date = DateUtils.round(date, Calendar.HOUR)
-        return date.time
+        return date.time + TimeUnit.HOURS.toMillis(1)
+        /*
+        For debug purpose we choose to send notification every round hour.
+        If we wanted to send it every morning at 9h00, we would to
+        date = DateUtils.round(data, Calender.DAY)
+        return dateTime + TimeUnit.HOURS.toMillis(9)
+         */
     }
 }
